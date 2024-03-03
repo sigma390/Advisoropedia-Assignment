0d2551d0d12ee5d5e72d863e4d13083665f70ead
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-03 12:25:14 +0530

    Added Some Functionality

--- Client/Advisoropedia-Frontend/src/Components/Login/Login.tsx
+++ Client/Advisoropedia-Frontend/src/Components/Login/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 
 
 const baseURL =  'http://localhost:3000/user';
@@ -9,6 +10,8 @@ const baseURL =  'http://localhost:3000/user';
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [passChnage, setPassChange]  =useState('password');
+  const navigate = useNavigate()
   
 
   const handleUsernameChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
@@ -20,8 +23,22 @@ const Login = () => {
   };
 
 
-  const handleResetPassword = () => {
-    setPassword('');
+
+  const handleSignup = ()=>{
+    navigate('/signup')
+  }
+
+
+  const handleShowPassword = () => {
+    if (passChnage=== 'email') {
+      setPassChange("password");
+
+      
+    }else{
+      setPassChange("email")
+
+    }
+    
   };
   const handleSubmit = async () => {
     // Form data (assuming you have state variables for username and password)
@@ -37,6 +54,7 @@ const Login = () => {
       toast.success("Login Successfull!",{
         position:'top-center'
       });
+      navigate("/posts")
       // Handle successful login response here
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -69,14 +87,19 @@ const Login = () => {
       <div className="input-group">
         <label htmlFor="password">Password (Min 6 characters)</label>
         <input
-          type="password"
+          type={passChnage}
           id="password"
           value={password}
           onChange={handlePasswordChange}
         />
-        <button className=' mt-1 hover:text-orange-500 duration-200' onClick={handleResetPassword}>Reset Password</button>
+        <div className=' flex justify-end'>
+        <button className=' mt-1 hover:text-orange-500 
+        duration-200' onClick={handleShowPassword}>Show Password</button>
+
+        </div>
+        
       </div>
-      <div className='flex mt-5 justify-center items-center'>
+      <div className='flex mt-2 justify-center items-center'>
       <button className=' w-32 flex p-2 bg-orange-700 
       rounded-xl justify-center items-center text-center
        text-xl text-white
@@ -86,6 +109,7 @@ const Login = () => {
 
 
       </div>
+      <div className='mt-2 flex justify-center items-center'><p>Dont have Account? </p><button onClick={handleSignup} className=' ml-2 text-blue-500'>Signup Here</button></div>
       
      
       
