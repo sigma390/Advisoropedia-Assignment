9fb8c6cd2e2db4500d2e49dac1584b241b83b4d6
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-05 10:45:37 +0530

    Input validation on loogin route proper error toast message !!!

--- Client/Advisoropedia-Frontend/src/Components/Login/Login.tsx
+++ Client/Advisoropedia-Frontend/src/Components/Login/Login.tsx
@@ -3,7 +3,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from 'react-router-dom';
-
+import { object, string,  ZodError } from 'zod';
 
 const baseURL =  'http://localhost:3000/user';
 
@@ -11,17 +11,18 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [passChnage, setPassChange]  =useState('password');
+  const [validationErrors, setValidationErrors] = useState<{ [key: string]: string }>({});
   const navigate = useNavigate()
   
-
-  const handleUsernameChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
+    clearValidationError('username');
   };
 
-  const handlePasswordChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
+    clearValidationError('password');
   };
-
 
 
   const handleSignup = ()=>{
@@ -40,39 +41,76 @@ const Login = () => {
     }
     
   };
+
+
+  //input validation
+  const schema = object({
+    username: string().email({ message: 'Please enter a valid email address' }),
+    password: string().min(6, { message: 'Password must be at least 6 characters long' }),
+  });
+
+
   const handleSubmit = async () => {
     // Form data (assuming you have state variables for username and password)
-    const formData = {
-      username: username,
-      password: password
-    };
+   
   
     try {
+      schema.parse({ username, password });
+      const formData = {
+        username: username,
+        password: password
+      };
       const response: AxiosResponse = await axios.post(baseURL+'/login', formData);
       
       console.log('Login response:', response.data);
+      localStorage.setItem("token",response.data.token)
       toast.success("Login Successfull!",{
         position:'top-center'
       });
       navigate("/posts")
       // Handle successful login response here
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        if (axiosError.response) {
-          console.error('Login error:', axiosError.response.data);
-          // Handle error response from the server
-        } else {
-          console.error('Login error:', axiosError.message);
-          // Handle other types of errors
+      
+
+        // Handle Zod validation errors
+        
+        if (error instanceof ZodError) {
+          const validationErrors: { [key: string]: string } = {};
+          error.errors.forEach((validationError) => {
+            const field = validationError.path.join('_');
+            validationErrors[field] = validationError.message;
+          });
+        setValidationErrors(validationErrors);
+
         }
+        // Handle axios errors
+        else if (axios.isAxiosError(error)) {
+          const axiosError = error as AxiosError;
+          if (axiosError.response) {
+            const responseData = axiosError.response.data as Record<string, unknown>;
+            const errorMessage = responseData.message;
+            toast.error(`${errorMessage}`, { position: 'top-center' });
+          } else {
+            console.error('Login error:', axiosError.message);
+            toast.error('An error occurred. Please try again later.', { position: 'top-center' });
+            
+          }
+        
       }
-       
     }
   };
+  const clearValidationError = (fieldName: string) => {
+    setValidationErrors((prevErrors) => {
+      const updatedErrors = { ...prevErrors };
+      delete updatedErrors[fieldName];
+      return updatedErrors;
+    });
+  };
+
 
   return (
     <>
+    <ToastContainer/>
      <div className=" shadow-lg shadow-slate-400 hover:scale-105 duration-200 signup-card ">
       <h2 className='text-2xl text-center hover:scale-110 duration-200' >Login</h2>
       <div className="input-group">
@@ -83,6 +121,9 @@ const Login = () => {
           value={username}
           onChange={handleUsernameChange}
         />
+        {validationErrors.username && (
+          <div className="error-message  text-red-600">{validationErrors.username}</div>
+        )}
       </div>
       <div className="input-group">
         <label htmlFor="password">Password (Min 6 characters)</label>
@@ -92,6 +133,9 @@ const Login = () => {
           value={password}
           onChange={handlePasswordChange}
         />
+        {validationErrors.password && (
+          <div className="error-message text-red-600">{validationErrors.password}</div>
+        )}
         <div className=' flex justify-end'>
         <button className=' mt-1 hover:text-orange-500 
         duration-200' onClick={handleShowPassword}>Show Password</button>
@@ -116,7 +160,7 @@ const Login = () => {
       
       
     </div>
-    <ToastContainer/>
+    
     
     
     </>
